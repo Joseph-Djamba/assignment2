defe5335e8388aa92216ceb37efa8615b3ba351d
jeefumba <jeefrader2@gmail.com>
2021-05-15 11:28:52 +0200

    First commit

---
+++ src/main/java/za/ac/cput/assignment2/Student.java
@@ -0,0 +1,47 @@
+package za.ac.cput.assignment2;
+
+public class Student {
+    private String studentNumber;
+    private String name;
+    private String surname;
+    private String student_class;
+
+    public Student(String studentNumber, String name, String surname, String student_class) {
+        this.studentNumber = studentNumber;
+        this.name = name;
+        this.surname = surname;
+        this.student_class = student_class;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStudent_class() {
+        return student_class;
+    }
+
+    public void setStudent_class(String student_class) {
+        this.student_class = student_class;
+    }
+}
